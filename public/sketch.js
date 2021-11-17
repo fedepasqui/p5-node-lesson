f4f771c8d63b8d3063d1bc45acc7c790a27544f1
fedepasqui <federica.pasquini@mail.polimi.it>
2021-11-17 13:13:00 +0100

    collaborative (red/yellow)

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,35 @@
+//activating socket also on the server side
+let clientSocket = io();
+
+clientSocket.on("connect", newConnection);
+clientSocket.on("mouseBroadcast", newBroadcast);
+
+function newConnection() {
+  console.log(clientSocket.id);
+}
+
+function newBroadcast(data) {
+  console.log(data);
+  fill("red");
+  circle(data.x, data.y, 10);
+}
+
 function setup() {
   createCanvas(400, 400);
+  background(255);
 }
 
 function draw() {
-  background(255);
+  fill("yellow");
   circle(mouseX, mouseY, 20);
+}
+
+//when i move the mouse, i send the information to the server which sends the info to the other clients
+function mouseMoved() {
+  let message = {
+    x: mouseX,
+    y: mouseY,
+  };
+
+  clientSocket.emit("mouse", message);
 }
--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 console.log("up and running");
 
+//uploaded express which is able to provide files to the server
 let express = require("express");
+
 //loading express
 let app = express();
 
 //creating a port to activate express
-let port = 3000;
+let port = process.env.PORT || 3000;
 
 //connection will happen on the port 3000
 let server = app.listen(port);
 
 console.log("server is running on http://localhost:" + port);
 
+//get items form the public folder
 app.use(express.static("public"));
+
+//import socket.io library
+let serverSocket = require("socket.io");
+
+//create a new object that will handle incoming and outcoming (io) messages
+let io = serverSocket(server);
+
+//a function that runs when a certain event happens, in this case is connection
+io.on("connection", newConnection);
+
+function newConnection(newSocket) {
+  console.log(newSocket.id);
+
+  //when you receive a message "kind of message" and execute the function mouseMessage()
+  newSocket.on("mouse", mouseMessage);
+
+  function mouseMessage(dataReceived) {
+    console.log(dataReceived);
+
+    //send the information back to all the other clients connected (broadcast)
+    newSocket.broadcast.emit("mouseBroadcast", dataReceived);
+  }
+}
