f22105b2f0f163b21ad3abd09ed7b85a30bbe7a2
fedepasqui <federica.pasquini@mail.polimi.it>
2021-11-17 11:33:44 +0100

    added sketch

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 console.log("up and running");
+
+let express = require("express");
+//loading express
+let app = express();
+
+//creating a port to activate express
+let port = 3000;
+
+//connection will happen on the port 3000
+let server = app.listen(port);
+
+console.log("server is running on http://localhost:" + port);
+
+app.use(express.static("public"));
